e43a1225707a8eff457f3799e8e038cd5d03c41c
das08 <takeda5955@gmail.com>
2021-10-25 22:23:53 +0900

    add: エラーの追加

---
+++ lccompiler/errors.py
@@ -0,0 +1,24 @@
+class REG_ERROR(Exception):
+    def __init__(self, msg: str) -> None:
+        super(REG_ERROR, self).__init__(msg)
+
+
+class REG_CONSTRUCT_ERROR(REG_ERROR):
+    def __init__(self, msg: str) -> None:
+        super(REG_CONSTRUCT_ERROR, self).__init__(msg)
+
+
+class REG_DECLARATION_ERROR(REG_ERROR):
+    def __init__(self, msg: str) -> None:
+        super(REG_DECLARATION_ERROR, self).__init__(msg)
+
+
+class IMM_ERROR(Exception):
+    def __init__(self, msg: str) -> None:
+        super(IMM_ERROR, self).__init__(msg)
+
+
+class IMM_CONSTRUCT_ERROR(IMM_ERROR):
+    def __init__(self, msg: str) -> None:
+        super(IMM_CONSTRUCT_ERROR, self).__init__(msg)
+
