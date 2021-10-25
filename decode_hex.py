6046ae418b08a7bd0a1e11f8930239a000ec741b
das08 <takeda5955@gmail.com>
2021-10-25 13:58:55 +0900

    initial commit

---
+++ decode_hex.py
@@ -0,0 +1,37 @@
+OP = {
+    "0000": "MOV A, {0}(0x{1})",
+    "0001": "ADD A, {0}(0x{1})",
+    "0010": "MOV A, B",
+    "0011": "MOV A, IN",
+    "0100": "MOV B, {0}(0x{1})",
+    "0101": "ADD B, {0}(0x{1})",
+    "0110": "MOV B, A",
+    "0111": "MOV B, IN",
+    "1000": "MOV OUT, {0}(0x{1})",
+    "1001": "MOV OUT, B",
+    "1010": "MOV B, GPR[{0}]",
+    "1011": "MOV GPR[{0}], B",
+    "1100": "SUB A, B",
+    "1101": "MULT A, B",
+    "1110": "JNC {0}(0x{1})",
+    "1111": "JMP {0}(0x{1})"
+
+}
+
+
+def convertFromHex(codes: list):
+    for i in range(len(codes)):
+        code = codes[i]
+        opcode = '{:04b}'.format(int(code[0], 16))
+        print(OP[opcode].format(int(code[1], 16), code[1]))
+
+    return
+
+
+sample_code = [
+    "0F",
+    "1E",
+    "2D"
+]
+
+convertFromHex(sample_code)
